56d2f9fc276d3c265939f8328f0a2d2e971cf403
cyh <1779726113@qq.com>
2025-08-20 16:32:48 +0800

    20

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import router from './router/index.ts'
+import 'element-plus/dist/index.css'
+import { ElImage } from 'element-plus'
 const app = createApp(App)
 app.use(router)
 app.use(ElementPlus)
+app.use(ElImage)
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,32 +28,70 @@
 		<div class="main-content">
 			<!-- 回到顶部 -->
 			<el-backtop :right="100" :bottom="100" />
-			
-			<!-- 内容区域 -->
-			<div class="content-list">
-				<el-row>
-					<el-col
-						v-for="(o, index) in 2"
-						:key="o"
-						:span="8"
-						:offset="index > 0 ? 2 : 0"
-					>
-						<el-card :body-style="{ padding: '0px' }">
-							<img
-								src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-								class="image"
-							/>
-							<div style="padding: 14px">
-								<span>汉堡</span>
-								<div class="bottom">
-									<time class="time">{{ currentDate }}</time>
-									<el-button type="text" class="button">操作</el-button>
+			<el-row>
+				<!-- 内容区域 -->
+				<div class="content-list flex">
+					<el-col :span = '18'>
+						<!-- 左侧文章卡片 -->
+						<div class="article-card">
+							<div class="card-image">
+								<el-image style="width: 100%; height: 100%;" :src="url" :fit="fit"/>
+							</div>
+							<div class="card-content">
+								<h3 class="article-title">博客部署教程-宝塔面板</h3>
+								<div class="article-meta">
+									<span class="meta-item">📌 置顶</span>
+									<span class="meta-item">📅 发表于 2023-05-19 19:47:44</span>
+									<span class="meta-item">🔄 更新于 2025-06-15 14:51:40</span>
+								</div>
+								<div class="article-tags">
+									<span class="tag">博客部署</span>
+									<span class="tag">linux</span>
+									<span class="tag">阿里云轻量服务器</span>
+									<span class="tag">宝塔面板</span>
+								</div>
+								<div class="article-stats">
+									<span>👍 116</span>
+									<span>👁 1.5w</span>
+								</div>
+								<p class="article-excerpt">这篇文章是在第一篇普通教程的基础上，使用宝塔面板...</p>
+							</div>
+						</div>
+					</el-col>
+					<el-col :span = '6'>
+						<!-- 右侧个人信息 -->
+						<div class=" about-me">
+							<el-image :src="url" :fit="fit"/>
+							<el-avatar class="avatar" shape="circle" size="large" :src="url" />
+							<h5>小灰的个人博客</h5>
+							<div class="pub about-me-content">
+								<p>👋 写出<i>HelloWord你就可以拿高薪了</i></p>
+							</div>
+							<div class="pub my-data">
+								<div class="pub-item">
+									<p>📖 文章116</p>
+								</div>
+								<div class="pub-item">
+									<p>👍 点赞量116</p>
+								</div>
+								<div class="pub-item">
+									<p>🎉 阅读量1.5w</p>
 								</div>
 							</div>
-						</el-card>
+							<div class="my-tags">
+								<button class="custom-gitee-btn">
+									<el-icon class="icon" size="18">
+										<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+											<path fill="currentColor" d="M512 1024C229.222 1024 0 794.778 0 512S229.222 0 512 0s512 229.222 512 512-229.222 512-512 512z m259.149-568.883h-290.74a25.293 25.293 0 0 0-25.292 25.293l-0.026 63.206c0 13.952 11.315 25.293 25.267 25.293h177.024c13.978 0 25.293 11.315 25.293 25.267v12.646a75.853 75.853 0 0 1-75.853 75.853h-240.23a25.293 25.293 0 0 1-25.267-25.293V417.203a75.853 75.853 0 0 1 75.827-75.853h353.946a25.293 25.293 0 0 0 25.267-25.292l0.077-63.207a25.293 25.293 0 0 0-25.268-25.293H417.152a189.62 189.62 0 0 0-189.62 189.645V771.15c0 13.977 11.316 25.293 25.294 25.293h372.94a170.65 170.65 0 0 0 170.65-170.65V480.384a25.293 25.293 0 0 0-25.293-25.267z"/>
+										</svg>
+									</el-icon>
+									My Gitee
+								</button>
+							</div>
+						</div>
 					</el-col>
-				</el-row>
-			</div>
+				</div>	
+			</el-row>
 		</div>
 	</div>
 </template>
@@ -63,7 +101,10 @@ import { ref } from 'vue'
 import '../assets/style/index.css'
 import { ArrowDownBold, } from '@element-plus/icons-vue'
 
-const currentDate = ref(new Date())
+// 图片地址 - 正确的静态资源引用方式
+import bgImage from '../assets/images/shunsea1.jpg'
+const url = ref(bgImage)
+const fit = ref('cover')
 </script>
 
 <style scoped lang="scss">
