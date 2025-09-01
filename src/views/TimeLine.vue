502163776703fb55252621e736d7fb4c32fb9edf
CHEN-YH99 <1779726113@qq.com>
2025-09-01 17:32:25 +0800

    update

--- src/views/TimeLine.vue
+++ src/views/TimeLine.vue
@@ -20,7 +20,7 @@
          v-for="(article, index) in pagedArticles"
          :key="article._id || ((currentPage - 1) * pageSize + index)"
          :class="{ reverse: (((currentPage - 1) * pageSize + index) % 2) === 1 }"
-         :timestamp="article.publishDate"
+         :timestamp="formatDate(article.publishDate)"
          :hollow="true" 
          center
          placement="top"
@@ -78,6 +78,24 @@ const {
   pageSize,
   total
 } = useArticles()
+
+// 时间格式化
+// 日期格式化函数
+const formatDate = (dateString: string | Date | undefined): string => {
+  if (!dateString) return '暂无日期'
+  
+  const date = new Date(dateString)
+  if (isNaN(date.getTime())) return '无效日期'
+  
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  // const hours = String(date.getHours()).padStart(2, '0')
+  // const minutes = String(date.getMinutes()).padStart(2, '0')
+  // const seconds = String(date.getSeconds()).padStart(2, '0')
+  // return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+   return `${year}-${month}-${day}`
+}
 
 watch([currentPage, pageSize], async () => {
   await nextTick()
